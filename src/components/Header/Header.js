bcf3d3db30f961cd6221750a52678c2d0c65a1ab
awongCM <awong.cm@gmail.com>
2019-02-23 15:52:35 +1100

    updated title bar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import AppBar from "material-ui/AppBar";
 
-import './Header.css';
+import "./Header.css";
 
 class Header extends Component {
-
-	render() {
-		return (
-			<AppBar className="Header" title={this.props.currentAudio.trackName} showMenuIconButton={false} />
-		);
-	}
+  render() {
+    return (
+      <AppBar
+        className="Header"
+        title={this.props.currentAudio.trackName || "Itune Music Player"}
+        showMenuIconButton={false}
+      />
+    );
+  }
 }
 
 export default Header;
